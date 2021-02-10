dd66bf76320414f5f26667d0723a9619163a2a76
Yuliana Alvarado <yalvarado924@gmail.com>
2021-02-10 17:34:27 -0600

    commit tree d999bd69f794b41d201b22d7dceefff8657e6737

---
+++ db/migrate/20210210233121_create_dogs.rb
@@ -0,0 +1,14 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    def up
+      create_table :dogs do |t|
+        t.string :name
+        t.string :breed
+      end
+    end
+  
+    def down
+      drop_table :dogs
+    end
+  end
+end
